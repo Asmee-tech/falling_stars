001e6c93c51d0719e1030976ff65e9bee5afb57b
Asmee-tech <asmee.khimani@gmail.com>
2025-09-13 09:26:23 +0200

    Update falling_stars.py

--- falling_stars.py
+++ falling_stars.py
@@ -6,6 +6,8 @@ s=Actor("star.png")
 b=Actor("basket.png")
 b.pos=500,740
 score=0
+lives=10
+game_over=False
 def place_star():
     s.x=randint(50,WIDTH-50)
     s.y=0
@@ -14,8 +16,13 @@ def draw():
   s.draw()
   b.draw()
   screen.draw.text(str(score),(5,5),fontsize=40,color="pink")
+  screen.draw.text(str(lives),(955,5),fontsize=40,color="red")
+  if game_over:
+    screen.fill("white")
+    screen.draw.text(("Game Over!"),(200,370),fontsize=150,color="red")
+    screen.draw.text(str(score),(500,500),fontsize=80,color="blue")
 def update():
-  global score
+  global score, lives
   s.y+=10
   if s.y>800:
     place_star()
@@ -23,8 +30,16 @@ def update():
     b.x-=10
   if keyboard.right:
     b.x+=10
-  if s.colliderect(b):
+  if s.colliderect(b) and not game_over:
     score+=1
     place_star()
+  if s.y>798:
+    lives-=1
+    if lives==0:
+      gameo()
+    place_star()
+def gameo():
+  global game_over
+  game_over=True
 place_star()
 pgzrun.go()
